96a1d018fb54b92504e9127a05828b43bdbb2e90
unknown <crazsich@gmail.com>
2022-06-13 00:14:20 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+// Declare the variable returnFirstTwoDrivers with const and assign an anonymous function to it. 
+
+// The assigned function should accept an array of drivers as an argument and return the first two drivers in the array.
+
+    const returnFirstTwoDrivers = function (drivers) {  
+
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+    console.log(drivers.slice(0, 2))
+   
+
+// Declare a variable called selectingDrivers .
+// This is an array containing two elements: 
+// The two functions that we previously defined (returnFirstTwoDrivers() and returnLastTwoDrivers()).createFareMultiplier()
+
+    const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+ }
+
+ 
